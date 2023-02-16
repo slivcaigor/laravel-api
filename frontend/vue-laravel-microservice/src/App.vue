9e586633d2324c346098364984bc394e40890deb
slivcaigor <slivcaigor@gmail.com>
2023-02-16 14:54:43 +0100

    Vue Install

---
+++ frontend/vue-laravel-microservice/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <main>
+        <HelloWorld />
+    </main>
+</template>
+
+<style scoped>
+main {
+    font-size: 50px;
+}
+</style>
